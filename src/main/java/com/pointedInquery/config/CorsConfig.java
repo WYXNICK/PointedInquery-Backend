f3149760983627922da8d9a6f99d1dbb484e7f49
WYXNICK <1685952374@qq.com>
2023-12-18 19:23:34 +0800

    Update CorsConfig.java

--- src/main/java/com/pointedInquery/config/CorsConfig.java
+++ src/main/java/com/pointedInquery/config/CorsConfig.java
@@ -2,25 +2,22 @@ package com.pointedInquery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-
-    // 当前跨域请求最大有效时长。这里默认1天
-    private static final long MAX_AGE = 24 * 60 * 60;
-
     @Bean
     public CorsFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*"); // 允许所有来源访问
+        corsConfig.addAllowedMethod("*"); // 允许所有HTTP方法
+        corsConfig.addAllowedHeader("*"); // 允许所有请求头
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:8081"); // 1 设置访问源地址
-        //corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 1 设置访问源地址
-        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
-        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
-        corsConfiguration.setMaxAge(MAX_AGE);
-        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置
-        return new CorsFilter(source);
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter((CorsConfigurationSource) source);
     }
 }
