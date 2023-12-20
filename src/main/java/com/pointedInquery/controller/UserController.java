ae119f28c16d0b509c77890b10d3e54ae3c6b7b3
WYXNICK <1685952374@qq.com>
2023-12-20 13:34:03 +0800

    修改用户注册接口

--- src/main/java/com/pointedInquery/controller/UserController.java
+++ src/main/java/com/pointedInquery/controller/UserController.java
@@ -65,7 +65,7 @@ public class UserController {
 		User user=new User();
 		//刚注册用户不会有行家权限
 		user.setPhone(userId);
-//		user.setIsexpert("否");
+		user.setIsExpert(0);
 		//对密码进行加密
 		user.setPassword(getMD5Str(passwd));
 		if(userService.save(user))
